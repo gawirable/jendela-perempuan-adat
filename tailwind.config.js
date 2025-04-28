0528263c9fc9eeb37e2d93e519ed459ad0c22a9e
gawirable <wirdan.ardi.r@gmail.com>
2025-04-28 11:42:29 +0700

    feat: add new color palette and content cards section
    Introduce a new color palette in the Tailwind configuration and implement a content cards section in the HTML. The section includes multiple cards with dynamic content and a call-to-action section to encourage user participation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ tailwind.config = {
         colors: {
           'multatuli-yellow': '#FFD94D',
           'multatuli-black': '#000',
+          'multatuli-pink': '#FFD6D6',
+          'multatuli-blue': '#C8F1FF',
+          'multatuli-green': '#E6FFD5',
+          'multatuli-gray': '#E5E5E5',
+          'multatuli-lightgray': '#EDEDED',
+          'jpa-red': '#FF6F5B',
+          'jpa-green': '#065143',
+          'jpa-purple': '#7B4D6E',
+          'jpa-yellow': '#FFD600',
+          'jpa-brown': '#A16A2B',
+          'jpa-brick': '#D34E24',
+          'jpa-mint': '#008B8B',
         }
       }
     }
