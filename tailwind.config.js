a01fadb266e4ed2fffeb85dd3b2d26bb9ae6c902
gawirable <wirdan.ardi.r@gmail.com>
2025-04-29 11:11:36 +0700

    feat: add about page and update collaboration page layout
    Add a new "About" page (`page-tentang.html`) and refactor the layout of the collaboration page (`page-kolaborasi.html`) for better consistency and readability. Updated the Tailwind config to adjust color values and improve styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        fontFamily: {
-          heading: ['"Tenor Sans"', 'sans-serif'],
-          body: ['Martel', 'serif'],
-        },
-        colors: {
-          'multatuli-yellow': '#FFD94D',
-          'multatuli-black': '#000',
-          'multatuli-pink': '#FFD6D6',
-          'multatuli-blue': '#C8F1FF',
-          'multatuli-green': '#E6FFD5',
-          'multatuli-gray': '#E5E5E5',
-          'multatuli-lightgray': '#EDEDED',
-          'jpa-red': '#FF6F5B',
-          'jpa-green': '#065143',
-          'jpa-purple': '#7B4D6E',
-          'jpa-yellow': '#FFD600',
-          'jpa-brown': '#A16A2B',
-          'jpa-brick': '#D34E24',
-          'jpa-mint': '#008B8B',
-          'jpa-yellow2': '#FFE180',
-        }
-      }
-    }
-  }
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ['"Tenor Sans"', "sans-serif"],
+        body: ["Martel", "serif"],
+      },
+      colors: {
+        "multatuli-yellow": "#FFD94D",
+        "multatuli-black": "#000",
+        "multatuli-pink": "#FFD6D6",
+        "multatuli-blue": "#C8F1FF",
+        "multatuli-green": "#E6FFD5",
+        "multatuli-gray": "#E5E5E5",
+        "multatuli-lightgray": "#EDEDED",
+        "jpa-red": "#FF6F5B",
+        "jpa-green": "#065143",
+        "jpa-purple": "#690045",
+        "jpa-yellow": "#FFD600",
+        "jpa-brown": "#A16A2B",
+        "jpa-brick": "#D34E24",
+        "jpa-mint": "#008B8B",
+        "jpa-yellow2": "#FFE180",
+      },
+    },
+  },
+};
